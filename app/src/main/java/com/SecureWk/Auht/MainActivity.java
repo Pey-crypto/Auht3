bde19ab23b6b41b0dfc35c925ec761e26af8e6c8
Pey-crypto <53893419+Pey-crypto@users.noreply.github.com>
2020-07-20 01:38:10 +0530

    Location to added

--- app/src/main/java/com/SecureWk/Auht/MainActivity.java
+++ app/src/main/java/com/SecureWk/Auht/MainActivity.java
@@ -3,20 +3,33 @@ package com.SecureWk.Auht;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.biometric.BiometricPrompt;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
 import java.util.concurrent.Executor;
 
 public class MainActivity extends AppCompatActivity {
     private Executor executor;
     private BiometricPrompt biometricPrompt;
     private BiometricPrompt.PromptInfo promptInfo;
+    private FusedLocationProviderClient fusedLocationClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         executor = ContextCompat.getMainExecutor(this);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         biometricPrompt = new BiometricPrompt(MainActivity.this,
                 executor, new BiometricPrompt.AuthenticationCallback() {
             @Override
@@ -28,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
                         .show();
                 System.exit(1);
             }
+
             @Override
             public void onAuthenticationSucceeded(
                     @NonNull BiometricPrompt.AuthenticationResult result) {
@@ -52,5 +66,28 @@ public class MainActivity extends AppCompatActivity {
                 .setConfirmationRequired(false)
                 .build();
         biometricPrompt.authenticate(promptInfo);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        Toast.makeText(getApplicationContext(), "Got Location", Toast.LENGTH_SHORT)
+                                .show();
+                        if (location != null) {
+                            Toast.makeText(getApplicationContext(),"k", Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    }
+                });
     }
 }
